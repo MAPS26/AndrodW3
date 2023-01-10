1cb8bbb17159cebbf90b3683d2fb07c9ee32143b
Edgar Carreño Fonseca <edgarcarrenofonseca@outlook.com>
2023-01-10 02:08:49 -0600

    sync repo

---
+++ app/src/main/java/com/example/parte_2_semana_3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.parte_2_semana_3;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.parte_2_semana_3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
